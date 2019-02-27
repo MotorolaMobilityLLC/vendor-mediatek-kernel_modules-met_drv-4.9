a7ffbd07c0857b2e310419b73c72ff5f5ead5582
weichuan.lin <weichuan.lin@mediatek.com>
2019-02-27 18:58:37 +0800

    [ALPS04065592] MET: support get cpu voltage from sspm
    [Detail]
    1. choose cpu voltage from ap or sspm by ap_ptpod_enable node
    2. output gpu voltage by gpu_volt_enable node
    3. use workqueue to polling by get_volt_by_wq node
    
    MTK-Commit-Id: 06f9dcbb422c6e8cc176b6202b44a41fec620813
    
    Change-Id: I4dfc941d18799575cc5cb751e6b9808f60255770
    CR-Id: ALPS04065592
    Feature: [Module]MET (Mediatek Embedded Technology)

--- common/met_ptpod.c
+++ common/met_ptpod.c
@@ -22,22 +22,51 @@
 #include "core_plf_init.h"
 #include "core_plf_trace.h"
 
-static int ptpod_started;
-//static unsigned int g_u4CPUVolt_LL;
+
 static unsigned int MT_GPU_DVFS_IDX = NR_MT_CPU_DVFS;
-static unsigned int g_u4Volt[NR_MT_CPU_DVFS + 1];
 static unsigned int g_u4GPUVolt;
+static unsigned int g_u4Volt[NR_MT_CPU_DVFS + 1];
+
+/* g_ap_ptpod: cpu volt from ap or sspm setting
+ * if 1: cpu volt from ap
+ * if 0: cpu volt from sspm */
+static unsigned int g_ap_ptpod;
+
+/* gpu_volt_enable:
+ * if 1: enable gpu volt to output
+ * if 0: disable gpu volt to output */
+static unsigned int gpu_volt_enable = 1;
+
+/* get_volt_by_wq:
+ * if 1: get cpu/gpu volt by workqueue
+ * if 0: get cpu/gpu volt in irq */
+static unsigned int get_volt_by_wq;
+
+static int ptpod_started;
+static struct kobject *kobj_ptpod;
+static struct delayed_work get_volt_dwork;
 
 noinline void ms_ptpod(void)
 {
-//	MET_TRACE("%u,%u\n", g_u4CPUVolt_LL, g_u4GPUVolt);
-	char	*SOB, *EOB;
+	char *SOB, *EOB;
 
-	g_u4Volt[MT_GPU_DVFS_IDX] = g_u4GPUVolt;
+	if (g_ap_ptpod) {
+		MET_TRACE_GETBUF(&SOB, &EOB);
 
-	MET_TRACE_GETBUF(&SOB, &EOB);
-	EOB = ms_formatD_EOL(EOB, ARRAY_SIZE(g_u4Volt), g_u4Volt);
-	MET_TRACE_PUTBUF(SOB, EOB);
+		if (gpu_volt_enable) {
+			g_u4Volt[MT_GPU_DVFS_IDX] = g_u4GPUVolt;
+			EOB = ms_formatD_EOL(EOB, ARRAY_SIZE(g_u4Volt), g_u4Volt);
+		} else
+			EOB = ms_formatD_EOL(EOB, ARRAY_SIZE(g_u4Volt) - 1, g_u4Volt);
+
+		MET_TRACE_PUTBUF(SOB, EOB);
+	} else {
+		if (gpu_volt_enable) {
+			MET_TRACE_GETBUF(&SOB, &EOB);
+			EOB = ms_formatD_EOL(EOB, 1, &g_u4GPUVolt);
+			MET_TRACE_PUTBUF(SOB, EOB);
+		}
+	}
 }
 
 #if 0
@@ -70,30 +99,146 @@ static void ptpod_gpu_voltSampler(unsigned int a_u4Volt)
 }
 #endif
 
+#define PTPOD_CONF_SHOW_IMPLEMENT(var) \
+	do { \
+		int i; \
+		i = snprintf(buf, PAGE_SIZE, "%d\n", var); \
+		return i; \
+	} while (0)
+
+#define PTPOD_CONF_STORE_IMPLEMENT(var) \
+	do { \
+		int value; \
+		if ((n == 0) || (buf == NULL)) \
+			return -EINVAL; \
+		if (kstrtoint(buf, 0, &value) != 0) \
+			return -EINVAL; \
+		if (value == 1) \
+			var = 1; \
+		else \
+			var = 0; \
+		return n; \
+	} while (0)
+
+
+static ssize_t ap_ptpod_enable_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	PTPOD_CONF_SHOW_IMPLEMENT(g_ap_ptpod);
+}
+
+static ssize_t ap_ptpod_enable_store(struct kobject *kobj,
+			struct kobj_attribute *attr,
+			const char *buf,
+			size_t n)
+{
+	PTPOD_CONF_STORE_IMPLEMENT(g_ap_ptpod);
+}
+
+static ssize_t gpu_volt_enable_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	PTPOD_CONF_SHOW_IMPLEMENT(gpu_volt_enable);
+}
+
+static ssize_t gpu_volt_enable_store(struct kobject *kobj,
+			struct kobj_attribute *attr,
+			const char *buf,
+			size_t n)
+{
+	PTPOD_CONF_STORE_IMPLEMENT(gpu_volt_enable);
+}
+
+static ssize_t get_volt_by_wq_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	PTPOD_CONF_SHOW_IMPLEMENT(get_volt_by_wq);
+}
+
+static ssize_t get_volt_by_wq_store(struct kobject *kobj,
+			struct kobj_attribute *attr,
+			const char *buf,
+			size_t n)
+{
+	PTPOD_CONF_STORE_IMPLEMENT(get_volt_by_wq);
+}
+
+static struct kobj_attribute ap_ptpod_enable_attr = __ATTR(ap_ptpod_enable, 0664, ap_ptpod_enable_show, ap_ptpod_enable_store);
+static struct kobj_attribute gpu_volt_enable_attr = __ATTR(gpu_volt_enable, 0664, gpu_volt_enable_show, gpu_volt_enable_store);
+static struct kobj_attribute get_volt_by_wq_attr = __ATTR(get_volt_by_wq, 0664, get_volt_by_wq_show, get_volt_by_wq_store);
+
+/* create ptpod related kobj node */
+#define KOBJ_ATTR_LIST \
+	do { \
+		KOBJ_ATTR_ITEM(ap_ptpod_enable); \
+		KOBJ_ATTR_ITEM(gpu_volt_enable); \
+		KOBJ_ATTR_ITEM(get_volt_by_wq); \
+	} while (0)
+
 static int ptpod_create(struct kobject *parent)
 {
+	int ret = 0;
+
+	kobj_ptpod = parent;
+
+#define KOBJ_ATTR_ITEM(attr_name) \
+	do { \
+		ret = sysfs_create_file(kobj_ptpod, &attr_name ## _attr.attr); \
+		if (ret != 0) { \
+			pr_notice("Failed to create " #attr_name " in sysfs\n"); \
+			return ret; \
+		} \
+	} while (0)
+	KOBJ_ATTR_LIST;
+#undef  KOBJ_ATTR_ITEM
+
 	return 0;
 }
 
 static void ptpod_delete(void)
 {
+#define KOBJ_ATTR_ITEM(attr_name) \
+	sysfs_remove_file(kobj_ptpod, &attr_name ## _attr.attr)
+
+	if (kobj_ptpod != NULL) {
+		KOBJ_ATTR_LIST;
+		kobj_ptpod = NULL;
+	}
+#undef  KOBJ_ATTR_ITEM
 }
 
-static void met_ptpod_polling(unsigned long long stamp, int cpu)
+static void update_volt_value(void)
 {
 	int i;
 
-	if (mt_cpufreq_get_cur_volt_symbol) {
+	if (g_ap_ptpod && mt_cpufreq_get_cur_volt_symbol) {
 		/*
 		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_LL)/100;
 		g_u4CPUVolt_L = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_L)/100;
 		g_u4CPUVolt_CCI = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_CCI)/100;
 		*/
-//		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(0)/100;
-
 		for (i = 0; i < NR_MT_CPU_DVFS; i++)
 			g_u4Volt[i] = mt_cpufreq_get_cur_volt_symbol(i) / 100;
 	}
+
+	if (gpu_volt_enable) {
+		if (mt_gpufreq_get_cur_volt_symbol)
+			g_u4GPUVolt = ((mt_gpufreq_get_cur_volt_symbol() + 50) / 100);
+	}
+}
+
+static void get_volt_notify(unsigned long long stamp, int cpu)
+{
+	schedule_delayed_work(&get_volt_dwork, 0);
+}
+
+static void met_ptpod_polling_by_wq(struct work_struct *work)
+{
+	update_volt_value();
+
+	ms_ptpod();
+}
+
+static void met_ptpod_polling(unsigned long long stamp, int cpu)
+{
+	update_volt_value();
 
 	ms_ptpod();
 }
@@ -107,26 +252,19 @@ static void ptpod_start(void)
 	met_gpufreq_setvolt_registerCB(ptpod_gpu_voltSampler, kFOR_MET_PTPOD_USE);
 #endif
 
-	int i;
+	if (get_volt_by_wq) {
+		met_ptpod.timed_polling = get_volt_notify;
 
-	if (mt_cpufreq_get_cur_volt_symbol) {
-		/*
-		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_LL)/100;
-		g_u4CPUVolt_L = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_L)/100;
-		g_u4CPUVolt_CCI = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_CCI)/100;
-		*/
-//		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(0)/100;
-		for (i = 0; i < NR_MT_CPU_DVFS; i++)
-			g_u4Volt[i] = mt_cpufreq_get_cur_volt_symbol(i) / 100;
-	}
+		INIT_DELAYED_WORK(&get_volt_dwork, met_ptpod_polling_by_wq);
+	} else
+		met_ptpod.timed_polling = met_ptpod_polling;
 
-	if (mt_gpufreq_get_cur_volt_symbol)
-		g_u4GPUVolt = ((mt_gpufreq_get_cur_volt_symbol()+50)/100);
+	update_volt_value();
 
 	ms_ptpod();
 
-	/* register callback */
 #if 0
+	/* register callback */
 	if (mt_cpufreq_setvolt_registerCB_symbol)
 		mt_cpufreq_setvolt_registerCB_symbol(ptpod_cpu_voltSampler);
 #endif
@@ -139,29 +277,18 @@ static void ptpod_start(void)
  */
 static void ptpod_stop(void)
 {
-	int i;
-
 	ptpod_started = 0;
 
-	/* unregister callback */
 #if 0
+	/* unregister callback */
 	if (mt_cpufreq_setvolt_registerCB_symbol)
 		mt_cpufreq_setvolt_registerCB_symbol(NULL);
 #endif
 
-	if (mt_cpufreq_get_cur_volt_symbol) {
-		/*
-		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_LL)/100;
-		g_u4CPUVolt_L = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_L)/100;
-		g_u4CPUVolt_CCI = mt_cpufreq_get_cur_volt_symbol(MT_CPU_DVFS_CCI)/100;
-		*/
-//		g_u4CPUVolt_LL = mt_cpufreq_get_cur_volt_symbol(0)/100;
-		for (i = 0; i < NR_MT_CPU_DVFS; i++)
-			g_u4Volt[i] = mt_cpufreq_get_cur_volt_symbol(i) / 100;
-	}
+	if (get_volt_by_wq)
+		cancel_delayed_work_sync(&get_volt_dwork);
 
-	if (mt_gpufreq_get_cur_volt_symbol)
-		g_u4GPUVolt = (mt_gpufreq_get_cur_volt_symbol() + 50) / 100;
+	update_volt_value();
 
 	ms_ptpod();
 
@@ -180,27 +307,54 @@ static int ptpod_print_help(char *buf, int len)
 /*
  * It will be called back when run "met-cmd --extract" and mode is 1
  */
-//static char header[] =
-//	"met-info [000] 0.0: ms_ud_sys_header: ptpod,CPUVolt_LL,GPUVolt,d,d\n";
 static int ptpod_print_header(char *buf, int len)
 {
-	int i;
 	int str_len = 0;
 
-	str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "met-info [000] 0.0: met_ptpod_header: ");
+	if (g_ap_ptpod) {
+		int i;
 
-	for (i = 0; i < NR_MT_CPU_DVFS; i++)
-		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "CPUVolt_%d,", i);
+		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+						"met-info [000] 0.0: met_ptpod_header: ");
 
-	str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "GPUVolt,");
+		for (i = 0; i < NR_MT_CPU_DVFS; i++)
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "CPUVolt_%d,", i);
 
-	for (i = 0; i < NR_MT_CPU_DVFS + 1; i++)
-		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "d,");
+		if (gpu_volt_enable)
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "GPUVolt,");
 
-	buf[str_len - 1] = '\n';
+		for (i = 0; i < NR_MT_CPU_DVFS; i++)
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "d,");
+
+		if (gpu_volt_enable)
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "d,");
+
+		buf[str_len - 1] = '\n';
+
+		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+							"met-info [000] 0.0: met_ptpod_version: ap\n");
+	} else {
+		if (gpu_volt_enable) {
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+							"met-info [000] 0.0: met_ptpod_header: ");
+
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "GPUVolt,");
+			str_len += snprintf(buf + str_len, PAGE_SIZE - str_len, "d,");
+			buf[str_len - 1] = '\n';
+		}
+
+		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+							"met-info [000] 0.0: met_ptpod_version: sspm\n");
+	}
+
+	if (gpu_volt_enable)
+		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+						"met-info [000] 0.0: met_ptpod_gpu_volt_enable: YES\n");
+	else
+		str_len += snprintf(buf + str_len, PAGE_SIZE - str_len,
+						"met-info [000] 0.0: met_ptpod_gpu_volt_enable: NO\n");
 
 	return str_len;
-//	return snprintf(buf, PAGE_SIZE, header);
 }
 
 struct metdevice met_ptpod = {
