70a2c2fc9f8690b732f398cdd88f80fcf706e7c0
weichuan.lin <weichuan.lin@mediatek.com>
2019-02-27 18:55:12 +0800

    [ALPS04065592] MET: disable insert met module during boot on user load
    [Detail]
    before use met, need to get root permission first,
    so no permission issue on user load
    
    MTK-Commit-Id: c09c0fd74485ada1c67bb1a4d01fe59913d88595
    
    Change-Id: I36eddcdbdd80a5ac070a652994fbcfcbab22c3f9
    CR-Id: ALPS04065592
    Feature: [Module]MET (Mediatek Embedded Technology)

--- Android.mk
+++ Android.mk
@@ -8,13 +8,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := met.ko
 LOCAL_STRIP_MODULE := true
 
-ifeq (user,$(TARGET_BUILD_VARIANT))
-    MET_INTERNAL_USE := $(shell test -f $(MY_KERNEL_ROOT_DIR)/vendor/mediatek/kernel_modules/met_drv_secure/4.9/init.met.rc && echo yes)
-    ifeq ($(MET_INTERNAL_USE),yes)
-        LOCAL_INIT_RC := ../../met_drv_secure/4.9/init.met.rc
-    endif
-endif
-
 include $(MTK_KERNEL_MODULE)
 
 endif # Kernel version matches current path
